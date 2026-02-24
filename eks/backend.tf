2fa811ec8970fe1d7888ca65c3dfd9a0846912d8
Damir Abdurakhimov <109365159+Damir94@users.noreply.github.com>
2026-02-23 21:00:34 -0500

    Update Terraform required version and AWS provider

--- eks/backend.tf
+++ eks/backend.tf
@@ -1,31 +1,33 @@
 terraform {
-  required_version = "1.14.5"
+  required_version = "~> 1.9.3"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.49.0"
+      version = "~> 5.49"
     }
   }
-  resource "aws_s3_bucket" "terraform_state_bucket" {
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_s3_bucket" "terraform_state_bucket" {
   bucket = "dev-damir-tf-bucket"
 
   lifecycle {
     prevent_destroy = false
   }
 }
+
 resource "aws_dynamodb_table" "terraform_state_lock" {
   name         = "Lock-Files"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
+
   attribute {
     name = "LockID"
     type = "S"
-   }
- }
-}
-
-
-
-provider "aws" {
-  region = var.aws-region
+  }
 }
